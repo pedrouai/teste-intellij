7c0984279be0904a5cec1fe9bd76228598728bcb
Pedro Batista <pedrobatista2038@gmail.com>
2023-02-26 23:14:43 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+       /*  SAÍDA DE DADOS - EXEMPLOS
+        *
+        *  String nome = "Pedro";
+        *  float nota = 8.5f;
+        *
+        *  System.out.print("A nota é: " + nota);
+        *  System.out.println("A nota é: " + nota);
+        *  System.out.printf("A nota é: %.1f", nota);
+        *  System.out.printf("A nota de %s é %.1f. \n", nome, nota);
+        *  System.out.format("A nota de %s é %.1f. \n", nome, nota);
+        *
+        */
+    }
+}
